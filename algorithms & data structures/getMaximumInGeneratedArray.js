55da2967ed55d531b8ee5d6313452be9220ad3ff
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-11-09 22:28:01 -0800

    added toy problem

---
+++ algorithms & data structures/getMaximumInGeneratedArray.js
@@ -0,0 +1,18 @@
+// 1646. Get Maximum in Generated Array
+/**
+ * You are given an integer n. An array nums of length n + 1 is generated in the following way:
+
+nums[0] = 0
+nums[1] = 1
+nums[2 * i] = nums[i] when 2 <= 2 * i <= n
+nums[2 * i + 1] = nums[i] + nums[i + 1] when 2 <= 2 * i + 1 <= n
+Return the maximum integer in the array nums​​​.
+
+ * @param {number} n
+ * @return {number}
+ */
+
+
+var getMaximumGenerated = function(n) {
+
+};
