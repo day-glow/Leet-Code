8763b86a855ea39fb90a28065e8f3ffa014be56c
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-11-17 12:00:20 -0800

    twoSum ioce's

--- blind75/twoSum.js
+++ blind75/twoSum.js
@@ -10,4 +10,14 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
+ g- return index of two nums that sum total to target
+
+ i- array (numbers) - unsorted
+ o- array (2 numbers) of index location of input arr
+ se- n/a
+ c- O(n2) exponential
+    input arr length?, nums[i] limit?, target limit?
+    only one answer exists.
+ ec- input arr length = 0, neg nums,
+
 */
