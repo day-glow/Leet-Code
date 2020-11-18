867d6ee0ff5aa11649e79d99b652ec890209685e
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-11-18 11:06:50 -0800

    ioce's

--- blind75/isPalindrome.js
+++ blind75/isPalindrome.js
@@ -6,4 +6,25 @@ Note: For the purpose of this problem, we define empty string as valid palindrom
 Ex 1:
 Input: "A man, a plan, a canal: Panama"
 Output: true
+
+g- determine if a string contains the same letters forward, as backward from center. (read forward or backward is the same phrase/word)
+
+i- word/phrase (string)
+o- boolean
+se- n/a
+c- not case sensitive, any limit to string length?
+   TC - naive O(n) - linear
+ec- spaces, punctuation, casing
+   palindrome is odd/even length
+
+viz -
+"Racecar" --> true
+
+-change string to same case
+-remove punctuation
+-length/2 = center
 */
+
+
+
+
