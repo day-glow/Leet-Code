b4d4740b3e891d7c6e8adba36946a7bec492203c
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-01 15:08:29 -0800

    ioce's

--- blind75/missingNumber.js
+++ blind75/missingNumber.js
@@ -9,3 +9,16 @@ Output: 2
 Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 
 */
+
+/*
+ //return missing number in the range
+ //sort, take last num (highest), and find missing
+ //sum all nums and find missing value (0 to highest sum difference)
+ nums are consecutive starting at 0
+
+ i-array of nums
+ o-single num
+ se-n/a, may sort og arr or could make copy
+ c-O(n)
+ ec- if no "missing" in range, then next increment +1 is the missing num, if arr only contains 0 & no n
+*/
