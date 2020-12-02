aa92a8b38dd4d1ab9c88ac97cdac35357dbef6a2
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-02 15:49:36 -0800

    optimized solution using Math.min and Math.max

--- blind75/bestTimeToBuyAndSellStock.js
+++ blind75/bestTimeToBuyAndSellStock.js
@@ -43,3 +43,18 @@ const maxProfit = (prices) => {
 */
 
 //OPTIMIZED
+const maxProfit = prices => {
+  if (prices.length < 1) {
+    return 0;
+  }
+
+  let profit = 0;
+  let buy = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    buy = Math.min(buy, prices[i]);
+    profit = Math.max(profit, prices[i] - buy);
+  }
+
+  return profit;
+};
