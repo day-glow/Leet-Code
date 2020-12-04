f8e36922cb12f4743eb3ec15fa48721527a4cfd3
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-03 17:27:03 -0800

    using recursion

--- blind75/mergeTwoSortedLists.js
+++ blind75/mergeTwoSortedLists.js
@@ -12,3 +12,16 @@ Example 3:
 Input: l1 = [], l2 = [0]
 Output: [0]
 */
+
+const mergeTwoLists = (l1, l2) => {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
