e2b4cd4a212f2a6b3f1841acedbdf19978602c9c
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-04 11:39:06 -0800

    ioce's, TC/SC

--- blind75/subtreeOfAnotherTree.js
+++ blind75/subtreeOfAnotherTree.js
@@ -13,4 +13,12 @@ Given tree t:
    4
   / \
  1   2
+
+   //trav over tree nodes (use recursion)
+    //check each node val and l&r, if not a match, return false
+    //if found, return true
+    //if not found, return false
+
+TC-O(n) or worse case O(n*m) must traverse both trees fully
+SC-O(n)
 */
