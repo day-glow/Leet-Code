a86cc5af4682f4f3388ab434dc2fb97278ccd541
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-08 12:24:04 -0800

    recursive DFS approach

--- blind75/sameTree.js
+++ blind75/sameTree.js
@@ -16,3 +16,12 @@ Output: true
 TC-O(n)
 SC-O(n)
 */
+
+//RECURSION, DFS:
+const isSameTree = (p, q) => {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
