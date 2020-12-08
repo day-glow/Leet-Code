dd76ae2c9073d588ad0c2cbe9aaf65aad0c2bba2
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-08 14:09:26 -0800

    refractored recursive approach, added iterative BFS (long approach)

--- blind75/sameTree.js
+++ blind75/sameTree.js
@@ -25,3 +25,61 @@ const isSameTree = (p, q) => {
 
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
+
+//Recursive refractored:
+const isSameTree = (p, q) => {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
+
+//ITERATIVE, BFS:
+//helper
+const isSameNode = (p, q) => {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  if (p.val !== q.val) return false;
+  return true;
+};
+
+const isSameTree = (p, q) => {
+  if (p === null && q === null) return true;
+  if (!isSameNode(p, q)) return false;
+
+  //create a queue
+  let queueP = [];
+  let queueQ = [];
+  //enqueue values
+  queueP.push(p);
+  queueQ.push(q);
+
+  //while loop
+  while (queueP.length > 0 || queueQ.length > 0) {
+    let pNode = queueP.shift();
+    let qNode = queueQ.shift();
+
+    //if values are not equal, return false
+    if (!isSameNode(p, q)) return false;
+    //if values are not null, push children into queue
+    if (pNode !== null) {
+      if (!isSameNode(pNode.left, qNode.left)) return false;
+      if (p.left !== null) {
+        queueP.push(pNode.left);
+        queueQ.push(qNode.left);
+      }
+      if (!isSameNode(pNode.right, qNode.right)) return false;
+      if (p.right !== null) {
+        queueP.push(pNode.right);
+        queueQ.push(qNode.right);
+      }
+    }
+  }
+  //return true
+  return true;
+};
+
+//JSON stringify
+var isSameTree = function(p, q) {
+  return JSON.stringify(p)===JSON.stringify(q)
+};
