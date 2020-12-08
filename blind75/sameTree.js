98e130ef2786238ae356b8f2e09de7d2501270fc
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-08 12:12:07 -0800

    blind75 Q, same tree

---
+++ blind75/sameTree.js
@@ -0,0 +1,16 @@
+/*
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+
+*/
