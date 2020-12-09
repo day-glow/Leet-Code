f7851ba7fe8101d2e10af07f1de99bc9e216e010
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-09 15:50:55 -0800

    pop and push method, TC/SC

--- easyProblems/reverseInteger.js
+++ easyProblems/reverseInteger.js
@@ -12,3 +12,23 @@ Input: x = 123
 Output: 321
 */
 
+//TC-O(logx)
+//SC-O(1)
+const reverse = (x) => {
+  let isPos = true;
+  let result = 0;
+
+  if (x < 0) {
+    x *= -1;
+    isPos = false;
+  }
+
+  while (x > 0) {
+    let endNum = x % 10;
+    x = Math.floor(x / 10);
+    result = (result * 10) + endNum;
+  }
+
+  if (result > Math.pow(2, 31) - 1) return 0;
+  return (isPos) ? result : -result;
+};
