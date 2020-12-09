922ceb8367b5967ba1c2925e3365332584bd1c78
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-09 10:49:51 -0800

    2 pointers, fast & slow

--- blind75/linkedListCycle.js
+++ blind75/linkedListCycle.js
@@ -14,3 +14,22 @@ Input: head = [3,2,0,-4], pos = 1
 Output: true
 Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
 */
+
+//TWO POINTERS APPROACH:
+//TC-O(n) --> O(n + k)
+//SC-O(1)
+const hasCycle = head => {
+  let p1 = head
+  let p2 = head
+
+  while (p2 && p2.next && p2.next.next) {
+      p1 = p1.next
+      p2 = p2.next.next
+
+      if (p1 === p2) {
+          return true
+      }
+  }
+
+  return false
+}
