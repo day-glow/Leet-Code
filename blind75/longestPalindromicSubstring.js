f4f666f7157fc67933e291705538f415136ab916
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-16 15:51:26 -0800

    simple loop, helper approach(center out)

--- blind75/longestPalindromicSubstring.js
+++ blind75/longestPalindromicSubstring.js
@@ -21,3 +21,25 @@ Output: "a"
 
 //TC-O(n^2)
 //SC-O(1)
+const longestPalindrome = s => {
+  let longestSubstring = '';
+
+  if (s.length === 0 || s.length === 1) return s;
+
+  const isPalindrome = (left, right) => {
+    while (left >= 0 && right <= s.length && s[left] === s[right]) {
+      if (right - left >= longestSubstring.length) {
+        longestSubstring = s.substring(left, right + 1);
+      }
+      left--;
+      right++;
+    }
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    isPalindrome(i, i);
+    isPalindrome(i, i + 1);
+  }
+
+  return longestSubstring;
+};
