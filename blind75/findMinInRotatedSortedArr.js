6ece3c6b284a30daaefcf66c16af37f22e22ee6d
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-17 15:07:14 -0800

    refractored binary search

--- blind75/findMinInRotatedSortedArr.js
+++ blind75/findMinInRotatedSortedArr.js
@@ -73,3 +73,18 @@ const findMin = nums => {
     return nums[left];
   }
 };
+
+//brute force, binary search (same approach but simplified)
+const findMin = nums => {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return nums[left];
+};
