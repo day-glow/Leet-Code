097a40b4f373757670cce5ad928d2cd20e51bf01
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-28 11:22:07 -0800

    bfs approach

--- blind75/binaryTreeLevelOrderTraversal.js
+++ blind75/binaryTreeLevelOrderTraversal.js
@@ -28,3 +28,27 @@ SC-
   avg- O(n)
   worst- O(n)
 */
+
+const levelOrder = root => {
+  if (root === null) return [];
+  let result = [];
+  let queue = [];
+  queue.push(root);
+
+  while (queue.length) {
+    let level = [];
+    let length = queue.length;
+    for (let i = 0; i < length; i++) {
+      let temp = queue.shift();
+      if (temp.left) {
+        queue.push(temp.left);
+      }
+      if (temp.right) {
+        queue.push(temp.right);
+      }
+      level.push(temp.val);
+    }
+    result.push(level);
+  }
+  return result;
+};
