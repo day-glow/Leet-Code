a5cacde9a54fbf0da2f5dc8ddd859269fcdaa0ca
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-28 18:25:31 -0800

    4 loops all going a directional flow, ugly and long

--- blind75/spiralMatrix.js
+++ blind75/spiralMatrix.js
@@ -15,12 +15,35 @@ Multiple loops for each col or row, then increase only one m or n at end of each
 */
 const spiralOrder = matrix => {
   let result = [];
-  //declare rows and column lengths
-  let rows = matrix.length;
-  let cols = matrix[0].length;
+  if (!matrix.length) return result;
 
-  let m = 0;
-  let n = 0;
+  //declare rows and column lengths
+  let rowsStart = 0;
+  let colsStart = 0;
+  let rowsEnd = matrix.length - 1;
+  let colsEnd = matrix[0].length - 1;
+
+
+  while (rowsStart <= rowsEnd && colsStart <= colsEnd) {
+    for (let c = colsStart; c <= colsEnd; c++) {
+      result.push(matrix[rowsStart][c]);
+    }
+    for (let r = rowsStart + 1; r <= rowsEnd; r++) {
+      result.push(matrix[r][colsEnd]);
+    }
+    if (rowsStart < rowsEnd && colsStart < colsEnd) {
+      for (let c = colsEnd - 1; c > colsStart; c--) {
+        result.push(matrix[rowsEnd][c]);
+      }
+      for (let r = rowsEnd; r > rowsStart; r--) {
+        result.push(matrix[r][colsStart]);
+      }
+    }
+    rowsStart++;
+    rowsEnd--;
+    colsStart++;
+    colsEnd--;
+  }
 
   return result;
 };
