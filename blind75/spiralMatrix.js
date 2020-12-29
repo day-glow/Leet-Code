5c36eac995a7fd68f00483c9f1480e8e732fe753
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-28 17:48:15 -0800

    TOPICS Q, medium

---
+++ blind75/spiralMatrix.js
@@ -0,0 +1,12 @@
+/*
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Ex1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Ex2:
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+*/
+
