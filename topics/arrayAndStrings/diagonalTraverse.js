aa69cb6c3511139b4186fde6cb4990a09e7cd23a
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-28 16:16:31 -0800

    nested for loops Brut force

--- topics/arrayAndStrings/diagonalTraverse.js
+++ topics/arrayAndStrings/diagonalTraverse.js
@@ -8,6 +8,7 @@ Input:
  [ 4, 5, 6 ],
  [ 7, 8, 9 ]
 ]
+coords: [0,0],[0,1],[1,0],[2,0],[1,1],[0,3],[1,3],[2,1],[3,3]
 
 Output:  [1,2,4,7,5,3,6,8,9]
 */
@@ -18,5 +19,21 @@ TC- O(n)
 SC- O(n)
 */
 const findDiagonalOrder = matrix => {
+  let diagonalNums = [];
+  if (matrix.length === 0 || matrix[0].length === 0) return diagonalNums;
+  let rows = matrix.length;
+  let columns = matrix[0].length;
 
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!diagonalNums[i + j]) {
+        diagonalNums[i + j] = [];
+      }
+      diagonalNums[i + j].push(matrix[i][j]);
+    }
+  }
+  for (let i = 0; i < (rows + columns - 1); i += 2) {
+    if (i % 2 === 0) diagonalNums[i] = diagonalNums[i].reverse();
+  }
+  return diagonalNums.flat();
 };
