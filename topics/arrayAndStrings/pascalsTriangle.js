8e53699cea4688fcff50dbcec6ffda51d5538f11
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-28 18:54:51 -0800

    brute force approach

--- topics/arrayAndStrings/pascalsTriangle.js
+++ topics/arrayAndStrings/pascalsTriangle.js
@@ -15,3 +15,29 @@ Output:
  [1,4,6,4,1]
 ]
 */
+
+//not efficient Brute Force:
+const generate = numRows => {
+
+  if (numRows === 0) return [];
+  let triangle = [];
+  triangle.push([1]);
+  if (numRows === 1) return triangle;
+
+  let currRow = 1;
+
+  while (triangle.length < numRows) {
+    let row = [];
+    row.push(1);
+    if (currRow > 1) {
+      for (let i = 0; i < triangle[currRow - 1].length - 1; i++) {
+        row.push(triangle[currRow - 1][i] + triangle[currRow - 1][i + 1]);
+      }
+    }
+    row.push(1);
+    triangle.push(row);
+    currRow++;
+  }
+
+  return triangle;
+};
