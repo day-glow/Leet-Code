8c1fddd1060d7e68ddd62ff7289cc9070d07833c
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-30 14:21:26 -0800

    hash table approach

--- topics/linkedLists/intersectionOfTwoLL.js
+++ topics/linkedLists/intersectionOfTwoLL.js
@@ -9,3 +9,26 @@ Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, 
 Output: Reference of the node with value = 8
 Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
 */
+
+//hash map
+//TC- O(n+m)
+//SC- O(m)
+const getIntersectionNode = (headA, headB) => {
+  if (!headA || !headB) return null;
+
+  let map = new Set();
+  let nodeA = headA;
+  let nodeB = headB;
+
+  while (nodeA) {
+    map.add(nodeA);
+    nodeA = nodeA.next;
+  }
+  while (nodeB) {
+    if (map.has(nodeB)) {
+      return nodeB;
+    }
+    nodeB = nodeB.next;
+  }
+  return null;
+};
