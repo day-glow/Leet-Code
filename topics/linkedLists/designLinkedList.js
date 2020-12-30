e8b1eb62edad550ce9a621b8a39c245999e47365
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-30 10:59:56 -0800

    .addAtIndex

--- topics/linkedLists/designLinkedList.js
+++ topics/linkedLists/designLinkedList.js
@@ -67,3 +67,22 @@ MyLinkedList.prototype.addAtTail = function(val) {
   this.size++;
   return this;
 };
+
+// Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+  let newNode = new Node(val);
+
+  if (this.size < index) return;
+  if (index <= 0) return this.addAtHead(val);
+  if (this.size === index) return this.addAtTail(val);
+
+  let currNode = this.head;
+    for (let i = 0; i < index - 1; i++) {
+    currNode = currNode.next;
+  }
+  let temp = currNode.next;
+  currNode.next = newNode;
+  newNode.next = temp;
+  this.size++;
+  return this;
+};
