5d6cad25731a5e5104d9f8ecd8bef7318d0d20b3
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-29 16:09:43 -0800

    .sort approach

--- topics/arrayAndStrings/arrayPartition.js
+++ topics/arrayAndStrings/arrayPartition.js
@@ -22,12 +22,11 @@ SC- O(logn)
 */
 const arrayPairSum = nums => {
   nums = nums.sort((a, b) => a - b);
- //current max
- let maxSum = 0;
+  let maxSum = 0;
 
- for (let i = 0; i < nums.length; i += 2) {
-   maxSum += nums[i];
- }
+  for (let i = 0; i < nums.length; i += 2) {
+    maxSum += nums[i];
+  }
 
- return maxSum;
+  return maxSum;
 };
