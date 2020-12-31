3c71f38abf00e04cf18c14d0e347d62ac11400f9
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-30 21:05:11 -0800

    one pass approach

--- topics/linkedLists/removeLinkedListElements.js
+++ topics/linkedLists/removeLinkedListElements.js
@@ -5,3 +5,27 @@ Example:
 Input:  1->2->6->3->4->5->6, val = 6
 Output: 1->2->3->4->5
 */
+
+//TC- O(N)
+//SC- O(1)
+const removeElements = (head, val) => {
+  if (!head) return head;
+  let node = head;
+  while (head) {
+    if (head.val === val) {
+      head = head.next;
+    } else {
+      break;
+    }
+  }
+
+  while (node && node.next) {
+    if (node.next.val === val) {
+      node.next = node.next.next ? node.next.next : null;
+    } else {
+      node = node.next;
+    }
+
+  }
+  return head;
+};
