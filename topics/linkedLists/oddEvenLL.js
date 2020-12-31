996294f634660ab9c4c329ee780ae123b4fe50b0
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-12-30 21:40:44 -0800

    2 heads && 2 pointers approach

--- topics/linkedLists/oddEvenLL.js
+++ topics/linkedLists/oddEvenLL.js
@@ -11,3 +11,21 @@ Example 2:
 Input: 2->1->3->5->6->4->7->NULL
 Output: 2->3->6->7->1->5->4->NULL
 */
+
+var oddEvenList = function(head) {
+  if (!head || !head.next || !head.next.next) return head;
+  let odd = head;
+  let even = head.next;
+  const evenHead = head.next;
+
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+
+  return head;
+};
