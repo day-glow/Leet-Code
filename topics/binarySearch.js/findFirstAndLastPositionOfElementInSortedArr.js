b26cdc41eba2f87340f283d9bd5fd8658084b0f2
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-05 11:01:50 -0800

    binary search split in two parts (left and right sub array checks)

--- topics/binarySearch.js/findFirstAndLastPositionOfElementInSortedArr.js
+++ topics/binarySearch.js/findFirstAndLastPositionOfElementInSortedArr.js
@@ -17,3 +17,31 @@ Example 3:
 Input: nums = [], target = 0
 Output: [-1,-1]
 */
+
+const searchRange = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  //search sub array left of pivot for start if pivot is >= target
+  while (left < right) {
+    let pivot = Math.floor((right - left) / 2) + left;
+    nums[pivot] >= target ? right = pivot : left = pivot + 1;
+  }
+
+  //if no start exists, then target doesn't exist since list if sorted
+  if (nums[left] !== target) return [-1, -1];
+
+  //save start index of target range & reset right
+  let start = left;
+  right = nums.length - 1;
+
+  //search sub array right for end of range
+  while (left < right) {
+    let pivot = Math.floor((right - left) / 2) + left;
+    nums[pivot] <= target ? left = pivot + 1 : right = pivot;
+  }
+
+  //if target was found
+  let end = nums[left] === target ? left : left - 1;
+  return [start, end];
+};
