bd5c2015680cb1bd52e970e17136fbc16a4e1fdf
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-05 12:32:07 -0800

    binary search optimized

--- topics/binarySearch.js/findKClosestElements.js
+++ topics/binarySearch.js/findKClosestElements.js
@@ -23,40 +23,13 @@ Output: [1,2,3,4]
 //[4,7,8,9]
 //will arr contain dups?
 
-//iterative
-
 //binary search, middle out OR
-//similar to range, find start and end
 const findClosestElements = (arr, k, x) => {
-  let closestInts = [];
   let left = 0;
-  let right = arr.length;
-
-  if (arr.length === k) return arr;
-  if (arr[left] >= x) return arr.slice(0, k);
-  if (arr[right] <= x) return arr.slice(-k);
-
+  let right = arr.length - 1;
   while (left < right) {
-    let pivot = Math.floor((right - left) / 2) + left;
-    if (arr[pivot] === x) {
-      if (k === 1) return [x];
-      closestInts.push(arr[pivot]);
-      left = pivot - 1;
-      right = pivot + 1;
-      break;
-    }
-    x - arr[pivot] > arr[pivot + k] - x ? left = pivot + 1 : right = pivot;
+      let pivot = Math.floor((right - left) / 2) + left;
+      x - arr[pivot] > arr[pivot + k]- x ? left = pivot + 1 : right = pivot;
   }
-
-  //check for k closest from pivot
-  while (closestInts.length < k) {
-    if (Math.abs(arr[left] - x) < Math.abs(arr[right] - x) || Math.abs(arr[left] - x) === Math.abs(arr[right] - x) && left < right) {
-      closestInts.unshift(arr[left]);
-      left--;
-    } else {
-      closestInts.push(arr[right]);
-      right++;
-    }
-  }
-  return closestInts;
+  return arr.slice(left, left + k);
 };
