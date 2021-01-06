0ab011153acc6927d358599cbb02b399143f380e
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-05 21:32:46 -0800

    recursive approach

--- topics/binarySearch.js/powXN.js
+++ topics/binarySearch.js/powXN.js
@@ -57,3 +57,23 @@ const myPow = (x, n) => {
 
   return ans;
 };
+
+//recursive
+const myPow = (x, n) => {
+  if (n === 1) return x;
+  if (n === 0) return 1;
+
+  if (n < 0) {
+    x = (1 / x);
+    n = -n;
+  }
+
+  if (n > 1) {
+    if (n % 2 === 0) {
+      return myPow(x * x, n / 2);
+    } else {
+      return x * myPow(x * x, (n - 1) / 2);
+    }
+  }
+  return x;
+};
