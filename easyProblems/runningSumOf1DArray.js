52eeac7ae673524e837581e492d5d6fe60616349
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-10 09:59:33 -0800

    inplace

--- easyProblems/runningSumOf1DArray.js
+++ easyProblems/runningSumOf1DArray.js
@@ -30,3 +30,13 @@ const runningSum = nums => {
   }
   return runSum;
 };
+
+//inplace
+const runningSum = nums => {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    nums[i] = sum;
+  }
+  return nums;
+};
