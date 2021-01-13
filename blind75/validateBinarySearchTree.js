340db37957715d35a663676ec23a9cab28ded0ed
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-12 20:47:55 -0800

    iterative, inorder traversal approach

--- blind75/validateBinarySearchTree.js
+++ blind75/validateBinarySearchTree.js
@@ -20,3 +20,36 @@ Input: root = [5,1,4,null,null,3,6]
 Output: false
 Explanation: The root node's value is 5 but its right child's value is 4.
 */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isValidBST = root => {
+  if (root.val === null || !root) return;
+  let stack = [];
+  let prev = null;
+
+  while (stack.length || root) {
+    while (root !== null) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    if (prev !== null && root.val <= prev) return false;
+
+    prev = root.val;
+    root = root.right;
+  }
+
+  return true;
+};
+
