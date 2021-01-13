9254020fe30afd830292c1da9e43b01779e4ac93
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-13 10:45:19 -0800

    first attempt queue approach (failing some test cases

--- topics/stacksAndQueues/perfectSquares.js
+++ topics/stacksAndQueues/perfectSquares.js
@@ -19,3 +19,32 @@ Explanation: 13 = 4 + 9.
 //n = target
 //similar to 2Sum/3Sum
   //make a perf squares map, check all perf squares less than n (target sum)
+
+var numSquares = function(n) {
+  let queue = [];
+  let fewestNums = 0;
+  let perfectSquares = new Set();
+
+  //find closest perfect square to n (target sum)
+  for (let i = n; i > 0; i--) {
+    if (Math.sqrt(i) % 2 === 0 || Math.sqrt(i) % 2 === 1) {
+      perfectSquares.add(i);
+      if (n - i === 0) return fewestNums + 1;
+      if (perfectSquares.has(n - i)) return fewestNums + 2;
+      queue.push(i);
+    }
+  }
+  //then check self & neighbors
+  while (queue.length) {
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let curr = queue.shift();
+      if (n - curr === 0) return fewestNums + 1;
+      if (perfectSquares.has(curr)) return fewestNums + 2;
+      queue.push(n - curr);
+    }
+    fewestNums++;
+  }
+  //if sq root % 2 = 0, it is a perfect square
+  return fewestNums;
+};
