44ee2c988b67fc905ce53ff3bd2f290160df88f1
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-14 16:41:53 -0800

    brainstorm

--- blind75/numberOfIslands.js
+++ blind75/numberOfIslands.js
@@ -21,3 +21,20 @@ Input: grid = [
 ]
 Output: 3
 */
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+//start with template then add what is missing
+//visiting all elems, DFS
+//BFS, checking neighbors
+
+  //iterate over grid elems
+  //check all neighbors, if all neighbors are 0's, increase island count. I
+  //f they are ones, continue and push neighbors into queue to be checked
+
+//how do we mark elems as seen? if connected island, not all neighbors will be 0's
+//visited coordinates or flag on elem as visited to not over count islands
+//continue to next unseen land 1 and continue checking.
