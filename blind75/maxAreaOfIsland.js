a6b840c304393595214b5a009ebcb0c723f3a0e8
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-20 15:48:57 -0800

    BFS approach

--- blind75/maxAreaOfIsland.js
+++ blind75/maxAreaOfIsland.js
@@ -18,3 +18,58 @@ Example 2:
 [[0,0,0,0,0,0,0,0]]
 Given the above grid, return 0.
 */
+
+/**
+//search for islands, if current island is greer than max, then replace
+//alter seen land to water to mark (or make a copy)
+*/
+
+//BFS approach
+const getNeighbors = (grid, x, y) => {
+  let neighbors = [];
+
+  let dirs = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ]
+
+  for (let [neighborX, neighborY] of dirs) {
+    if (neighborX + x >= 0 && neighborX + x < grid.length && neighborY + y >= 0 && neighborY + y < grid[0].length) {
+      if (grid[neighborX + x][neighborY + y]) neighbors.push([neighborX + x, neighborY + y]);
+    }
+  }
+  return neighbors;
+};
+
+const maxAreaOfIsland = grid => {
+  let max = 0;
+
+  let queue = [];
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      let currSize = 0;
+      if (grid[i][j] === 1) {
+        queue.push([i, j]);
+      }
+      while (queue.length) {
+        let [x, y] = queue.shift();
+        if (grid[x][y] === 1) {
+          currSize++;
+          grid[x][y] = 0;
+
+          let neighbors = getNeighbors(grid, x, y);
+          if (neighbors.length) {
+            for (let neighbor of neighbors) {
+                queue.push(neighbor);
+            }
+          }
+        }
+      }
+      max = currSize > max ? currSize : max;
+    }
+  }
+
+  return max;
+};
