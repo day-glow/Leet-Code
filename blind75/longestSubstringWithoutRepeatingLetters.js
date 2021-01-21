a61a1d6aaeaeafbdba04b380b7cc99c2eec321c5
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-20 20:59:14 -0800

    hash set approach

--- blind75/longestSubstringWithoutRepeatingLetters.js
+++ blind75/longestSubstringWithoutRepeatingLetters.js
@@ -17,3 +17,24 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+
+//Hash Set
+const lengthOfLongestSubstring = s => {
+  let longest = 0;
+  let letters = new Set();
+
+  let start = 0;
+  let end = 0;
+  while (start < s.length && end < s.length) {
+    if (!letters.has(s[end])) {
+      letters.add(s[end]);
+      end++;
+      longest = Math.max(longest, end - start);
+    } else {
+      letters.delete(s[start]);
+      start++;
+    }
+
+  }
+  return longest;
+};
