39a6b715719f8a4c0b00f7a309a08da5bcd809b5
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-22 11:07:04 -0800

    standard sliding window, keeping window valid

--- topics/slidingWIndow/maxConsecutiveOnesII.js
+++ topics/slidingWIndow/maxConsecutiveOnesII.js
@@ -11,3 +11,23 @@ Note:
 The input array will only contain 0 and 1.
 The length of input array is a positive integer and will not exceed 10,000
 */
+
+//standard catepillar sliding window, keeping invalid bounds
+const findMaxConsecutiveOnes = nums => {
+  let longest = 0;
+  let p1 = 0;
+  let p2 = 0;
+  let zeroCount = 0;
+
+  while (p2 < nums.length) {
+    let curr = nums[p2];
+    if (curr === 0) zeroCount++;
+    while (zeroCount > 1) {
+      if (nums[p1] === 0) zeroCount--;
+      p1++;
+    }
+    p2++;
+    longest = Math.max(longest, p2 - p1);
+  }
+  return longest;
+};
