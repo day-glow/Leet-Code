ca110772611f7c2395ccea8c2dc6df8ec8189b91
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-26 11:43:32 -0800

    first attempt O(n^2) TLE failed test case

--- blind75/productOfArrayExceptSelf.js
+++ blind75/productOfArrayExceptSelf.js
@@ -11,3 +11,17 @@ Note: Please solve it without division and in O(n).
 Follow up:
 Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
 */
+
+//first attempt
+//TC-O(n^2)
+//SC-O(n)
+//failed test case TLE
+const productExceptSelf = nums => {
+  let result = new Array(nums.length).fill(1);
+  let prev = 1;
+  for (let i = 0; i < nums.length; i++) {
+    [prev] = nums.splice(i, 1, prev);
+    nums.forEach(each => result[i] *= each);
+  }
+  return result;
+};
