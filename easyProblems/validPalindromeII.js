9877043cb73685e68cf7f7ae268c214c60d93836
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-31 21:15:37 -0800

    greedy approach

--- easyProblems/validPalindromeII.js
+++ easyProblems/validPalindromeII.js
@@ -10,3 +10,23 @@ Input: "abca"
 Output: True
 Explanation: You could delete the character 'c'.
 */
+
+//greedy approach (half for loop & valid palindrom helper)
+const isPalindrome = (s, start, end) => {
+  for (let i = start; i < start + (end - start) / 2; i++) {
+    if (s[i] !== s[end - i + start]) return false;
+  }
+  return true;
+}
+
+const validPalindrome = s => {
+  for (let i = 0; i < s.length / 2; i++) {
+    if (s[i] === s[s.length - 1 - i]) {
+      continue;
+    } else {
+      let j = s.length - 1 - i;
+      return (isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1));
+    }
+  }
+  return true;
+};
