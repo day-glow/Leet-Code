8c72021557d2cf90c679510374d7c79d80682bb5
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-02 11:00:42 -0800

    optimized sort approach with less space and no helper

--- blind75/mergeIntervals.js
+++ blind75/mergeIntervals.js
@@ -57,3 +57,20 @@ const merge = intervals => {
   mergedInt.push([prevMin, prevMax]);
   return mergedInt;
 };
+
+//OPTIMIZED
+const merge = intervals => {
+  if (intervals.length < 2) return intervals;
+  intervals.sort((a, b) => a[0] - b[0]);
+  let prev = intervals[0];
+  let res = [prev];
+  for (let curr of intervals) {
+    if (curr[0] <= prev[1]) {
+      prev[1] = Math.max(prev[1], curr[1]);
+    } else {
+      res.push(curr);
+      prev = curr;
+    }
+  }
+  return res;
+};
