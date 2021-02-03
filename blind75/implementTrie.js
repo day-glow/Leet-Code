6046fa3a0a329b2ae15dc17340fce9807484739f
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-03 11:10:48 -0800

    blind75 trie Q, medium

---
+++ blind75/implementTrie.js
@@ -0,0 +1,12 @@
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");
+trie.search("app");     // returns true
+*/
