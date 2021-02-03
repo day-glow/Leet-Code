eaaa4f72a6e5e8f66238a03448462bf1f3957a70
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-03 11:14:21 -0800

    object approach

--- blind75/implementTrie.js
+++ blind75/implementTrie.js
@@ -90,3 +90,46 @@ Trie.prototype.startsWith = function(prefix) {
   }
   return true;
 };
+
+//object approach
+var TrieNode = function(val = null) {
+  this.val = val;
+  this.isEnd = false;
+  this.children = {};
+};
+
+var Trie = function() {
+  //this.root = {};
+  this.root = new TrieNode();
+};
+
+Trie.prototype.insert = function(word) {
+  let node = this.root;
+  //iterate over word to insert
+  for (let currChar of word) {
+    if (!node.children[currChar]) {
+      let addLetter = new TrieNode(currChar);
+      node.children[currChar] = addLetter;
+    }
+    node = node.children[currChar];
+  }
+  node.isEnd = true;
+};
+
+Trie.prototype.search = function(word) {
+  let node = this.root;
+  for (let c of word) {
+    if (!node.children[c]) return false;
+    node = node.children[c];
+  }
+  return node.isEnd;
+};
+
+Trie.prototype.startsWith = function(prefix) {
+  let node = this.root;
+  for (let c of prefix) {
+    if (!node.children[c]) return false;
+    node = node.children[c];
+  }
+  return true;
+};
