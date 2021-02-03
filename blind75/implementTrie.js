5f2f1ccf61fa5a3be0a44530ca6d8bb14b6d632d
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-03 11:13:19 -0800

    hash map approach

--- blind75/implementTrie.js
+++ blind75/implementTrie.js
@@ -12,9 +12,6 @@ trie.search("app");     // returns true
 */
 
 //constructor class for TrieNode and Trie
-/**
- * Initialize your data structure here.
- */
 class TrieNode {
   constructor(val = null) {
     this.val = val;
@@ -24,20 +21,13 @@ class TrieNode {
 };
 
 class Trie {
-  //this.root = {};
   constructor() {
     this.root = new TrieNode();
   }
 };
 
-/**
- * Inserts a word into the trie.
- * @param {string} word
- * @return {void}
- */
 Trie.prototype.insert = function(word) {
   let node = this.root;
-  //iterate over word to insert
   for (let currChar of word) {
     if (!node.children.has(currChar)) node.children.set(currChar, new TrieNode(currChar));
     node = node.children.get(currChar);
@@ -45,11 +35,6 @@ Trie.prototype.insert = function(word) {
   node.isEnd = true;
 };
 
-/**
- * Returns if the word is in the trie.
- * @param {string} word
- * @return {boolean}
- */
 Trie.prototype.search = function(word) {
   let node = this.root;
   for (let c of word) {
@@ -59,11 +44,6 @@ Trie.prototype.search = function(word) {
   return node.isEnd;
 };
 
-/**
- * Returns if there is any word in the trie that starts with the given prefix.
- * @param {string} prefix
- * @return {boolean}
- */
 Trie.prototype.startsWith = function(prefix) {
   let node = this.root;
   for (let c of prefix) {
@@ -73,4 +53,40 @@ Trie.prototype.startsWith = function(prefix) {
   return true;
 };
 
-//
+//hash map approach
+var TrieNode = function(val = null) {
+  this.val = val;
+  this.isEnd = false;
+  this.children = new Map();
+};
+
+var Trie = function() {
+  this.root = new TrieNode();
+};
+
+Trie.prototype.insert = function(word) {
+  let node = this.root;
+  for (let currChar of word) {
+    if (!node.children.has(currChar)) node.children.set(currChar, new TrieNode(currChar));
+    node = node.children.get(currChar);
+  }
+  node.isEnd = true;
+};
+
+Trie.prototype.search = function(word) {
+  let node = this.root;
+  for (let c of word) {
+    if (!node.children.has(c)) return false;
+    node = node.children.get(c);
+  }
+  return node.isEnd;
+};
+
+Trie.prototype.startsWith = function(prefix) {
+  let node = this.root;
+  for (let c of prefix) {
+    if (!node.children.has(c)) return false;
+    node = node.children.get(c);
+  }
+  return true;
+};
