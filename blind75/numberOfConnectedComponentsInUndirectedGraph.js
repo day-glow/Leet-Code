58ed9007ff6e10dd66e731c58e0a3c0074f372fc
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-05 10:26:12 -0800

    brainstorm union find

--- blind75/numberOfConnectedComponentsInUndirectedGraph.js
+++ blind75/numberOfConnectedComponentsInUndirectedGraph.js
@@ -19,3 +19,23 @@ Input: n = 5 and edges = [[0, 1], [1, 2], [2, 3], [3, 4]]
 
 Output:  1
 */
+
+/*
+UNION FIND:
+//keep one "head node" as -1 for that connected component
+//assign connected nodes the "head node" it is connected to
+//all the -1's remaining are separated components
+//if there are two separte head nodes and those components are connect by edge, it will refer
+//to head node and change that -1 to the new head node.
+sets = {
+  0: -1,
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 3,
+}
+left = 0
+right = 3
+
+count = 0
+*/
