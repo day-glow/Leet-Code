ebf517445044c2dc1836d07089b57994b96e994d
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-09 18:59:46 -0800

    for each loop approach

--- blind75/meetingRooms.js
+++ blind75/meetingRooms.js
@@ -27,3 +27,15 @@ const canAttendMeetings = intervals => {
 //TC-O(n^2)
 //SC-O(1)
 //solve with nested loops and overlap conditions for each pair's vals
+
+//optional for each loop
+var canAttendMeetings = function(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  let prevEndTime = 0;
+  let canAttendAll = true;
+  intervals.forEach(meeting => {
+    if (meeting[0] < prevEndTime) canAttendAll = false;
+    prevEndTime = meeting[1];
+  })
+  return canAttendAll;
+};
