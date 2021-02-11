6362e1a6b789b3d48c9a08b756494ecea513b3d5
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-11 12:32:27 -0800

    optimized solution

--- blind75/mergeKSortedLists.js
+++ blind75/mergeKSortedLists.js
@@ -32,3 +32,38 @@ Output: []
 //create queue of lists, merging 2 at a time, only one remains
 //TC-O(nlogk)
 //SC-O(1)
+
+const mergeTwoLists = (l1, l2) => {
+  //merge in place or new list; let head = l1.val <= l2.val ? l1 : l2;
+  let head = new ListNode();
+  let node = head;
+  while (l1 !== null && l2 !== null) {
+    //node.next set to min of two lists, continue until null
+    if (l1.val <= l2.val) {
+      node.next = l1;
+      l1 = l1.next;
+    } else {
+      node.next = l2;
+      l2 = l2.next;
+    }
+    node = node.next;
+  }
+  if (l1 === null) node.next = l2;
+  if (l2 === null) node.next = l1;
+  return head.next;
+};
+
+var mergeKLists = function(lists) {
+  if (!lists.length) return null;
+
+  while (lists.length > 1) {
+    let list1 = lists.shift();
+    let list2 = lists.shift();
+
+    let mergedPair = mergeTwoLists(list1, list2);
+    lists.push(mergedPair);
+  };
+
+  return lists.shift();
+};
+
