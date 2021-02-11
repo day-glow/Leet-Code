2040b29560bd780634ad5aedb93699efc01fecfa
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-11 11:22:21 -0800

    refactored optimized solution

--- blind75/rotateImage.js
+++ blind75/rotateImage.js
@@ -136,3 +136,22 @@ const rotate = matrix => {
   transpose(matrix);
   reflect(matrix);
 };
+
+//refactored:
+const rotate = m => {
+  let n = m.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      let temp = m[i][j];
+      m[i][j] = m[j][i];
+      m[j][i] = temp;
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n / 2; j++) {
+      let temp = m[i][j];
+      m[i][j] = m[i][n - j - 1];
+      m[i][n - j - 1] = temp;
+    }
+  }
+};
