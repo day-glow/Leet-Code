f72e5cce0f1cc9b730b6881bb978b15319a719a6
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-18 20:45:40 -0800

    refactored optimized approach

--- blind75/setMatrixZeros.js
+++ blind75/setMatrixZeros.js
@@ -71,3 +71,24 @@ var setZeroes = function(matrix) {
     }
   }
 };
+
+//OPTIMIZED O(1) SPACE saving approach refractored
+var setZeroes = function(matrix) {
+  let isCol = false;
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] === 0) isCol = true;
+    for (let j = 1; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
+      }
+    }
+  }
+
+  for (let i = matrix.length - 1; i >= 0; i--) {
+    for (let j = matrix[0].length - 1; j > 0; j--) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) matrix[i][j] = 0;
+    }
+    if (isCol) matrix[i][0] = 0;
+  }
+};
