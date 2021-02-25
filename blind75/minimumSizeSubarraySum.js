20fe34cccf30677eedd340f4b548d0187416c8b5
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-02-24 16:49:52 -0800

    second pass two pointers one pass

--- blind75/minimumSizeSubarraySum.js
+++ blind75/minimumSizeSubarraySum.js
@@ -8,6 +8,25 @@ Output: 2
 Explanation: the subarray [4,3] has the minimal length under the problem constraint.
 */
 
+//second pass two pointers one pass:
+var minSubArrayLen = function(target, nums) {
+  let min;
+  let subSum = 0;
+  let p1 = 0;
+  let p2 = 0;
+
+  while (p2 < nums.length && p1 < nums.length) {
+    if (nums[p1] === target || nums[p2] === target) return 1;
+    subSum += nums[p2];
+    while (subSum >= target) {
+      min = (min === undefined) ? p2 - p1 + 1 : Math.min(min, p2 - p1 + 1);
+      subSum -= nums[p1++];
+    }
+    p2++;
+  }
+  return min === undefined ? 0 : min;
+};
+
 //TC- O(n)
 //SC- O(1)
 const minSubArrayLen = (s, nums) => {
