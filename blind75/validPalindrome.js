0d9659d1a08f414c0b9ff6b533a3bf8db7311e20
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-03-02 15:37:26 -1000

    second pass

--- blind75/validPalindrome.js
+++ blind75/validPalindrome.js
@@ -11,3 +11,14 @@ Input: s = "race a car"
 Output: false
 Explanation: "raceacar" is not a palindrome.
 */
+
+var isPalindrome = function(s) {
+  if (!s.length) return true;
+  const alphanum = s.toUpperCase().replace(/[^A-Z0-9]/gi, "");
+  let p1 = 0;
+  let p2 = alphanum.length - 1;
+  while (p1 <= p2) {
+    if (alphanum[p1++] !== alphanum[p2--]) return false;
+  }
+  return true;
+};
