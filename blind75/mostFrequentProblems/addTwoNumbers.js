e75cc16ffcd526ef19510d187e5062eacbbe747d
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-03-08 08:45:19 -1000

    first pass, using remainder approach

--- blind75/mostFrequentProblems/addTwoNumbers.js
+++ blind75/mostFrequentProblems/addTwoNumbers.js
@@ -16,3 +16,68 @@ Example 3:
 Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  let carry = 0;
+  let sum = new ListNode();
+  let head = sum;
+  //iterate over both LL, while loop
+  while (l1 && l2) {
+    carry += l1.val + l2.val;
+    if (carry < 9) {
+      sum.next = new ListNode(carry);
+      carry = 0;
+    } else {
+      sum.next = new ListNode(carry % 10);
+      carry = Math.floor(carry / 10);
+    }
+    sum = sum.next;
+    l1 = l1.next;
+    l2 = l2.next;
+  }
+  if (l1) {
+    while (l1) {
+      carry += l1.val;
+      if (carry < 10) {
+        sum.next = new ListNode(carry);
+        carry = 0;
+      } else {
+        sum.next = new ListNode(carry % 10);
+        carry = Math.floor(carry / 10);
+      }
+      sum = sum.next;
+      l1 = l1.next;
+    }
+  }
+  if (l2) {
+    while (l2) {
+      carry += l2.val;
+      if (carry < 10) {
+        sum.next = new ListNode(carry);
+        carry = 0;
+      } else {
+        sum.next = new ListNode(carry % 10);
+        carry = Math.floor(carry / 10);
+      }
+      sum = sum.next;
+      l2 = l2.next;
+    }
+  }
+  if (carry) {
+    sum.next = new ListNode(carry);
+  }
+
+  return head.next;
+};
