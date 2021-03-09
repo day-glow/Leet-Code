cd557b3a82cccded8687b727b28a4cfb058d05a8
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-03-09 08:14:49 -1000

    solution

--- blind75/mostFrequentProblems/robotBoundedInCircle.js
+++ blind75/mostFrequentProblems/robotBoundedInCircle.js
@@ -54,3 +54,14 @@ var isRobotBounded = function(instructions) {
   return true;
 };
 */
+
+//solution via discussion board
+var isRobotBounded = function(instructions) {
+  let [x, y, dx, dy] = [0, 0, 0, 1];
+  for(let i of instructions) {
+      if(i == 'R') [dx, dy] = [dy, -dx];
+      else if(i == 'L') [dx, dy] = [-dy, dx]
+      else [x, y] = [x + dx, y + dy];
+  }
+  return (!x && !y) || dx || dy != 1;
+};
