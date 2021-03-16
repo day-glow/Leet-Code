5111c23ad62342fab007a669be70ea6bb4edb0e6
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-03-16 13:29:20 -1000

    first pass

--- mostFrequentProblems/insertDeleteGetRandomO(1).js
+++ mostFrequentProblems/insertDeleteGetRandomO(1).js
@@ -26,3 +26,51 @@ randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contai
 randomizedSet.insert(2); // 2 was already in the set, so return false.
 randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
 */
+
+/**
+ * Initialize your data structure here.
+ */
+ var RandomizedSet = function() {
+  this.randomizedSet = new Set();
+};
+
+/**
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+  let valNotInSetPriorToInsert = true;
+  this.randomizedSet.has(val) ? valNotInSetPriorToInsert = false : this.randomizedSet.add(val);
+  return valNotInSetPriorToInsert;
+};
+
+/**
+ * Removes a value from the set. Returns true if the set contained the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+  if (this.randomizedSet.has(val)) {
+    this.randomizedSet.delete(val);
+    return true;
+  }
+  return false;
+};
+
+/**
+ * Get a random element from the set.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+  let currentSet = Array.from(this.randomizedSet);
+  return currentSet[Math.floor(Math.random() * currentSet.length)];
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
