e542fd42b0c9b00b007613c93f5836f940f24229
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-03-23 10:49:17 -1000

    20/37 test cases

--- airbnbMostCommon/pourWater.js
+++ airbnbMostCommon/pourWater.js
@@ -27,7 +27,7 @@ Output: [2,2,2,3,2,2,2]
 //left, -1
 //right, +1
 
-//first pass passing only 19/37 test cases
+//first pass passing only 20/37 test cases
 var pourWater = function(heights, V, K) {
   let waterPosition;
   while (V > 0) {
@@ -41,7 +41,9 @@ var pourWater = function(heights, V, K) {
 
 var getLowPoint = function(heights, start, increment) {
   let p = start + increment;
-  if (heights[p] < heights[start]) return p;
+  //if (heights[p] > heights[start]) return start;
+  while (heights[p + increment] && heights[p + increment] < heights[p]) p += increment;
   while (heights[p + increment] && heights[p] === heights[start]) p += increment;
+
   return heights[p] >= heights[start] ? start : p;
 };
