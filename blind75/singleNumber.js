a5a2cde6f3fe77ff469dda7251a91620e334eae1
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-03-25 13:08:45 -1000

    moved one liner to top and note for logic

--- blind75/singleNumber.js
+++ blind75/singleNumber.js
@@ -17,6 +17,9 @@ Example 3:
 Input: nums = [1]
 Output: 1
 */
+//oneliner:
+const singleNumber = nums => nums.reduce((acc, curr) => acc ^ curr);
+//XOR the same 2 numbers returns 0 (cancels out), so XOR all nums, the remaining output is the missing number
 
 //Bit Manipulation Approach:
 //logic: hash set * 2
@@ -48,7 +51,8 @@ const singleNumber = nums => {
   for (let n of nums) missingNum ^= set.has(n) ? n : n ^ n ^ n;
   return missingNum;
 };
-//refactored: const singleNumber = nums => {
+//refactored:
+const singleNumber = nums => {
   let missingNum = 0;
   for (let n of nums) missingNum ^= n;
   return missingNum;
