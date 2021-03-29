74d9665efa7c1d054cb152bf8b07385c7162fac8
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-03-29 13:35:38 -1000

    dp iterative approach

--- blind75/climbingStairs.js
+++ blind75/climbingStairs.js
@@ -21,6 +21,17 @@ Explanation: There are three ways to climb to the top.
 3. 2 steps + 1 step
 */
 
+//dynamic programming iteratively
+var climbStairs = function(n) {
+  if (n === 1) return n;
+  let dp = new Array(n + 1);
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
 
 //brainstorm
 //num of paths => optimization problem
