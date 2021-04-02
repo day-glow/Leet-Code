cea41b44fd7dd6a6a82afcb4f44ab00cc2b793a9
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-01 14:16:44 -1000

    optional approach

--- blind75/wordBreak.js
+++ blind75/wordBreak.js
@@ -64,3 +64,14 @@ var wordBreak = function(s, wordDict) {
   return dp[s.length];
 };
 
+//optional, more confusion option:
+var wordBreak = function(s, wordDict) {
+  let dp = Array(s.length).fill(false);
+  for (let i = 0; i < s.length; i++){
+    for (let word of wordDict){
+      let slice = s.slice(i + 1 - word.length, i + 1);
+      if (word === slice && ((i - word.length < 0) || dp[i - word.length])) dp[i] = true;
+    }
+  }
+  return dp[dp.length - 1];
+};
