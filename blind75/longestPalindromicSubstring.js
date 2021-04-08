560e8a3d9e60cf45b37828b92ef54a8652025650
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-07 20:53:43 -1000

    OPTIMIZED Expand Around Center Approach

--- blind75/longestPalindromicSubstring.js
+++ blind75/longestPalindromicSubstring.js
@@ -18,8 +18,32 @@ Example 4:
 Input: s = "ac"
 Output: "a"
 */
-//brute force, nest loops O(n^2)
-//helper function isPalindrome & work out to in O(n^2)
+//brute force, nest loops O(n^3)/O(1), pick all possible start & end points, then check each substring
+//Dynamic Programming O(n^2)/O(n^2), build a table of what we know (add two letters, check prev)
+//Expand Around the Center O(n^2)/O(1), helper function isPalindrome & work middle out O(n^2)
+
+//OPTIMIZED Expand Around Center Approach:
+var longestPalindrome = function(s) {
+  if (s.length <= 1) return s;
+  let start = 0;
+  let end = 0;
+  for (let i = 0; i < s.length; i++) {
+    let len = Math.max(expandAroundCenter(s, i, i), expandAroundCenter(s, i, i + 1));
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
+    }
+  }
+  return s.substring(start, end + 1);
+};
+
+var expandAroundCenter = function(s, p1, p2) {
+  while (p1 >= 0 && p2 < s.length && s[p1] === s[p2]) {
+    p1--;
+    p2++;
+  }
+  return p2 - p1 - 1;
+};
 
 //REFRACTORED:
 const longestPalindrome = s => {
