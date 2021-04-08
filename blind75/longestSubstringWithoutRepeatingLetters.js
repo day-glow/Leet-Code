f465b0322f8f90bb7340bad2fb126cdae40fd310
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-07 20:35:21 -1000

    kept optimized solution at top

--- blind75/longestSubstringWithoutRepeatingLetters.js
+++ blind75/longestSubstringWithoutRepeatingLetters.js
@@ -17,6 +17,23 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+
+//Hash Set (save letters and indices)
+//TC-O(n)
+//SC-O(min(m,n))
+const lengthOfLongestSubstring = s => {
+  let longest = 0;
+  let map = new Map();
+  for (let i = 0, j = 0; j < s.length; j++) {
+    if (map.has(s[j])) {
+      i = Math.max(map.get(s[j]), i);
+    }
+    longest = Math.max(longest, j - i + 1);
+    map.set(s[j], j + 1);
+  }
+  return longest;
+};
+
 //brainstorm:
 //brute force, find each one by one with nested loops O(n^2)/O(n)
 //2 pointers & set O(n)/O(n)
@@ -53,24 +70,6 @@ const lengthOfLongestSubstring = s => {
     }
 
   }
-  return longest;
-};
-
-//Hash Set (save letters and indices)
-//TC-O(n)
-//SC-O(min(m,n))
-const lengthOfLongestSubstring = s => {
-  let longest = 0;
-  let map = new Map();
-
-  for (let i = 0, j = 0; j < s.length; j++) {
-    if (map.has(s[j])) {
-      i = Math.max(map.get(s[j]), i);
-    }
-    longest = Math.max(longest, j - i + 1);
-    map.set(s[j], j + 1);
-  }
-
   return longest;
 };
 
