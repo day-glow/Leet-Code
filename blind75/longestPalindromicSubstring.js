d9381fcc467ea195f0ea418e18b77ec012c35e76
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-07 20:55:43 -1000

    commiting from main branch (forgot checkout)

--- blind75/longestPalindromicSubstring.js
+++ blind75/longestPalindromicSubstring.js
@@ -18,11 +18,37 @@ Example 4:
 Input: s = "ac"
 Output: "a"
 */
-//REFRACTORED:
+//brute force, nest loops O(n^3)/O(1), pick all possible start & end points, then check each substring
+//Dynamic Programming O(n^2)/O(n^2), build a table of what we know (add two letters, check prev)
+//Expand Around the Center O(n^2)/O(1), helper function isPalindrome & work middle out O(n^2)
+
+//OPTIMIZED Expand Around Center Approach:
+var longestPalindrome = function(s) {
+  if (s.length <= 1) return s;
+  let start = 0;
+  let end = 0;
+  for (let i = 0; i < s.length; i++) {
+    let len = Math.max(expandAroundCenter(s, i, i), expandAroundCenter(s, i, i + 1));
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
+    }
+  }
+  return s.substring(start, end + 1);
+};
+
+var expandAroundCenter = function(s, p1, p2) {
+  while (p1 >= 0 && p2 < s.length && s[p1] === s[p2]) {
+    p1--;
+    p2++;
+  }
+  return p2 - p1 - 1;
+};
+
+//REFRACTORED brute force:
 const longestPalindrome = s => {
   if (s.length <= 1) return s;
   let longestSubstring = '';
-
   const isPalindrome = (left, right) => {
     while (left >= 0 && right <= s.length && s[left] === s[right]) {
       if (right - left >= longestSubstring.length) longestSubstring = s.substring(left, right + 1);
@@ -30,7 +56,6 @@ const longestPalindrome = s => {
       right++;
     }
   };
-
   for (let i = 0; i < s.length; i++) {
     isPalindrome(i, i);
     isPalindrome(i, i + 1);
@@ -40,13 +65,11 @@ const longestPalindrome = s => {
 };
 
 
-//TC-O(n^2)
+//TC-O(n^3)
 //SC-O(1)
 const longestPalindrome = s => {
   let longestSubstring = '';
-
   if (s.length === 0 || s.length === 1) return s;
-
   const isPalindrome = (left, right) => {
     while (left >= 0 && right <= s.length && s[left] === s[right]) {
       if (right - left >= longestSubstring.length) {
@@ -63,4 +86,24 @@ const longestPalindrome = s => {
   }
 
   return longestSubstring;
+};
+
+//thirds:
+var longestPalindrome = function(s) {
+  if (s.length <= 1) return s;
+  let longest = '';
+
+  var isPalindrome = function(p1, p2) {
+    while (s[p1] === s[p2] && p1 >= 0 && p2 <= s.length) {
+      if (p2 - p1 >= longest.length) longest = s.substring(p1, p2 + 1);
+      p2++;
+      p1--;
+    };
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    isPalindrome(i, i);
+    isPalindrome(i, i + 1);
+  }
+  return longest;
 };
