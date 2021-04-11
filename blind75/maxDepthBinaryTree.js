5f4ffffed6e42e5ef3e78bac6c06c4092fd88a0b
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-11 10:53:07 -1000

    recursive approach

--- blind75/maxDepthBinaryTree.js
+++ blind75/maxDepthBinaryTree.js
@@ -80,3 +80,9 @@ var maxDepth = function(root) {
   }
   return level;
 };
+
+//recursive approach:
+var maxDepth = function(root) {
+  if (!root) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
