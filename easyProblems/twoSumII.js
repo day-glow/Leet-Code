b18f29689479e684632b7b55e1bcc9ab9ab94b6d
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-12 20:58:38 -1000

    refactored

--- easyProblems/twoSumII.js
+++ easyProblems/twoSumII.js
@@ -24,12 +24,10 @@ var twoSum = function(numbers, target) {
   let p1 = 0;
   let p2 = numbers.length - 1;
   while (numbers[p1] + numbers[p2] !== target) {
-    if (numbers[p1] + numbers[p2] > target) p2--;
-    if (numbers[p1] + numbers[p2] < target) p1++;
+    numbers[p1] + numbers[p2] > target ? p2-- : p1++;
   }
   return [p1 + 1, p2 + 1];
 };
-
 //hashMap approach:
 //O(n)
 //O(n)
