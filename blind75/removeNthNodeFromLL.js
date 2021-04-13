f37ec0cfe6d120cdeeb8c9224b640bc37246e67f
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-13 08:00:04 -1000

    added link to problem

--- blind75/removeNthNodeFromLL.js
+++ blind75/removeNthNodeFromLL.js
@@ -1,4 +1,6 @@
 /*
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
 Follow up: Could you do this in one pass? YES
