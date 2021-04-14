671783bfa4bbaefa4d9ec60373a40fa66cda6bc8
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-14 10:21:31 -1000

    check every rotation approach

--- easyProblems/totateString.js
+++ easyProblems/totateString.js
@@ -13,3 +13,9 @@ Example 2:
 Input: A = 'abcde', B = 'abced'
 Output: false
 */
+
+//check every rotation
+var rotateString = function(A, B) {
+  for (let i = 0; i < A.length; i++) if (A.slice(i) + A.slice(0, i) === B) return true;
+  return B.length ? false : true;
+};
