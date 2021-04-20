e421e84f637bf3f3eec78a7cdd808dcd40cb34a0
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-19 19:41:38 -1000

    interview ready, optimized

--- mockInterviews/fibonacciNumber.js
+++ mockInterviews/fibonacciNumber.js
@@ -22,18 +22,9 @@ Input: n = 4
 Output: 3
 Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 */
-
-//dp
-var fib = function(n) {
-  if (n < 2) return n;
-  let dp = new Array(n);
-  dp[0] = 0;
-  dp[1] = 1;
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
-  return dp[n];
-};
+//recursion, O(2^n)/O(n)
+//dp with memoization O(n)/O(n)
+//dp with 2 variables O(n)/O(1)
 
 //dp, constant space
 var fib = function(n) {
@@ -48,6 +39,14 @@ var fib = function(n) {
   return curr;
 };
 
-//brute force, loop O(n)/O(n^2)
-//recursion, O(n)/O(n^2)
-//dp O(n)/O(n)
+//dp, memoization
+var fib = function(n) {
+  if (n < 2) return n;
+  let dp = new Array(n);
+  dp[0] = 0;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
