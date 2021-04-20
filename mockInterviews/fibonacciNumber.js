69be9cc2dcb8f62a26d809d388866822e3c029f0
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-19 19:30:42 -1000

    mock approach

--- mockInterviews/fibonacciNumber.js
+++ mockInterviews/fibonacciNumber.js
@@ -22,3 +22,20 @@ Input: n = 4
 Output: 3
 Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 */
+
+//dp
+var fib = function(n) {
+  let total = 0;
+  let count = 0;
+  let dp = new Array(n);
+  dp[0] = 0;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+
+//brute force, loop O(n)/O(n^2)
+//recursion, O(n)/O(n^2)
+//dp O(n)/O(n)
