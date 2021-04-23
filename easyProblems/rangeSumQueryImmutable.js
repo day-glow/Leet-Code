d19a591e43aedaaf0eb9636cd9bb9c2617b08be6
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-22 17:50:14 -1000

    mock easy Q

---
+++ easyProblems/rangeSumQueryImmutable.js
@@ -0,0 +1,23 @@
+/*
+https://leetcode.com/problems/range-sum-query-immutable/
+
+Given an integer array nums, find the sum of the elements between indices left and right inclusive, where (left <= right).
+
+Implement the NumArray class:
+
+NumArray(int[] nums) initializes the object with the integer array nums.
+int sumRange(int left, int right) returns the sum of the elements of the nums array in the range [left, right] inclusive (i.e., sum(nums[left], nums[left + 1], ... , nums[right])).
+
+Example 1:
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+*/
