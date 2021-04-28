5a534407332b6a0e8361dbc9f0b3f76cdfd7530a
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-04-28 14:37:28 -0700

    thirds

--- blind75/productOfArrayExceptSelf.js
+++ blind75/productOfArrayExceptSelf.js
@@ -11,6 +11,21 @@ Note: Please solve it without division and in O(n).
 Follow up:
 Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
 */
+//O(n)/O(1) unless counting array of output
+//optimized:
+var productExceptSelf = function(nums) {
+  let products = new Array(nums.length).fill(1);
+  for (let i = 1; i < nums.length; i++) {
+    products[i] = products[i - 1] * nums[i - 1];
+  }
+  let prev = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    products[i] *= prev;
+    prev *= nums[i];
+  }
+  return products;
+};
+
 
 //first attempt
 //TC-O(n^2)
