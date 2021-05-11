f66050c07b9e460a0d98b0c641b2cafca76b8163
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-05-11 15:12:36 -0700

    partition labels Q

---
+++ amazonPrep/partitionLabels.js
@@ -0,0 +1,17 @@
+/*
+https://leetcode.com/problems/partition-labels/
+
+A string s of lowercase English letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+
+Example 1:
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+Note:
+s will have length in range [1, 500].
+s will consist of lowercase English letters ('a' to 'z') only.
+*/
