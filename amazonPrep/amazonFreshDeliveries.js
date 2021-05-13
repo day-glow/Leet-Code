7cdba1f9e92558c7b6d495518b01d2bf2af692d0
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-05-13 15:20:00 -0700

    fresh deliveries

--- amazonPrep/amazonFreshDeliveries.js
+++ amazonPrep/amazonFreshDeliveries.js
@@ -1,7 +1,10 @@
 /*
 https://leetcode.com/discuss/interview-question/1200709/Amazon-OAorFresh-Deliveries
 
-Amazon Fresh is a grocery delivery service that offers customers the option of purchasing their groceries online and schedule future deliveries of purchased deliveries. Amazon's backend system dynamically tracks each Amazon Fresh delivery truck and automatically assigns the next deliveries in a truck's plan. Top accomplish this, the system generates an optimized delivery plan with X destinations. The most optimizated plan would deliver to the closest X destinations from the start among all of the possible destinations in the plan.
+similar to k-closest points to the origin
+https://leetcode.com/problems/k-closest-points-to-origin/
+
+Amazon Fresh is a grocery delivery service that offers customers the option of purchasing their groceries online and schedule future deliveries of purchased deliveries. Amazon's backend system dynamically tracks each Amazon Fresh delivery truck and automatically assigns the next deliveries in a truck's plan. To accomplish this, the system generates an optimized delivery plan with X destinations. The most optimizated plan would deliver to the closest X destinations from the start among all of the possible destinations in the plan.
 
 Given a list of N possible delivery destinations, implement an algorithm to create the delivery plan for the closest X destinations.
 
@@ -13,7 +16,27 @@ Output: 2D integer array
 */
 
 var deliveryPlan = function(allLocations, numDeliveries) {
-  //write code here
+  if (!allLocations.length || !numDeliveries) return [];
+  let sortedByDist = allLocations.sort((a, b) => Math.sqrt((a[0] * a[0]) + (a[1] * a[1])) - Math.sqrt((b[0] * b[0]) + (b[1] * b[1])));
+  return sortedByDist.slice(0, numDeliveries);
 }
 
-//similar to k-closest points to the origin
+
+/*
+allLoc = [[x,y], [x,y], [x,y] ...]
+numDeliveries = 2 (find closest 2) "top K" algo
+
+start location seems to be in relation to the given options (assume 0,0)
+
+//top k closest to current point? Or closest to next new point
+assuming closest to current point
+
+1. sort by x, and y coords, difference between 2 points (find long leg)
+
+time O(nlogn) space O(1)
+
+//.sort makes most sense, another time intensive approach is to implement quickSelect sort
+
+*/
+
+
--- amazonPrep/robotBoundedInCircle.js
+++ amazonPrep/robotBoundedInCircle.js
@@ -30,3 +30,43 @@ Constraints:
 1 <= instructions.length <= 100
 instructions[i] is 'G', 'L' or, 'R'.
 */
+//4 limit trajectory, time O(n) space O(1)
+//mathmatical proof, time O(n) space O(1)
+
+/*
+Array standard for dirs
+               N     E     S       W
+directions: [[0,1],[1,0],[0,-1],[-1,0]];
+arrary idx:    0     1      2      3
+
+**Solution 4 cycles limit trajectory:
+after 4 cycles, robot must pass over (0,0) again to be in a circle
+
+directions: N,E,S,W
+coordinating steps: change direction or increase step in coords
+
+if no G's exist, robot stays still
+if no L's or R's exist, robot goes straight
+
+iterate over robot instructions and use counters for each direction
+  find the divisibilty and compare direction robot is facing. IF robot faces East or West, the robot will be in a circle.
+  if robot faces north, return false
+  if robot faces south....unsure
+
+Keep track for current coordinates and direction the robot is facing.
+Using logic to turn the robot 3rights = left (to stay in positive numbers)
+logic for ending coord/direction facing to return T/F
+          N
+          |
+          |
+          | X
+W--------------------E
+          |
+          |
+          |
+          S
+*/
+
+var isRobotBounded = function(string) {
+
+}
