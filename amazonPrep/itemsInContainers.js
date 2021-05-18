038a18fb487950e7fa4a923de45093d4e8d8b455
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-05-17 19:26:57 -0700

    items in container first pass

--- amazonPrep/itemsInContainers.js
+++ amazonPrep/itemsInContainers.js
@@ -15,9 +15,32 @@ For the second pair of indices, (1,6), the substring is '|**|*|' and there 2+1=3
 Both of the answers are returned in an array. [2,3].
 */
 
-class Result {
-	public static List<Integer> numberOfItems(s, startIndices, endIndices) {
-		// Write your code here
-
-		}
+var findCount = function(str) {
+  let p1 = 0;
+  let p2 = str.length - 1;
+  let count = 0;
+  while (str[p1] !== '|') p1++;
+  while (str[p2] !== '|') p2--;
+  while (p1 < p2) {
+    if (str[p1] === '*') count++;
+    p1++;
+  }
+  return count;
 }
+
+var numberOfItems = function(s, startIndices, endIndices) {
+  const numPairs = startIndices.length;
+  let itemsInCompartments = new Array(numPairs).fill(0);
+  for (let i = 0; i < numPairs; i++) {
+    itemsInCompartments[i] = findCount(s.substring(startIndices[i] - 1, endIndices[i]));
+  }
+  return itemsInCompartments;
+}
+
+const s = '|**|*|*';
+const startIndices = [1,1];
+const endIndices = [5,6];
+
+const expectedResult = [2,3];
+const actualResult = numberOfItems(s, startIndices, endIndices);
+console.log('expected: ', expectedResult, ', actual: ', actualResult);
